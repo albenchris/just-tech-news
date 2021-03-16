6c4513f34d14055b40f7d251f77f02a3d6367ac3
albenchris <albenchris00@gmail.com>
2021-03-16 13:49:51 -0500

    added helper functions to handlebars files

--- server.js
+++ server.js
@@ -27,7 +27,8 @@ app.use(session(sess));
 
 // handlebars engine
 const exphbs = require("express-handlebars");
-const hbs = exphbs.create({});
+const helpers = require("./utils/helpers");
+const hbs = exphbs.create({ helpers });
 
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
